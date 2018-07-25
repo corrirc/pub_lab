2e5b2b29118b82e9b04a173181483608a9ed2b85
Ricky <rickycorrigan@icloud.com>
2018-07-25 14:31:08 +0100

    Test pass for change of stock

--- pub.rb
+++ pub.rb
@@ -2,10 +2,11 @@ class Pub
 
 attr_reader :name, :till
 
-  def initialize(name, till, drinks)
+  def initialize(name, till, drinks, stock)
     @name = name
     @till = till
     @drinks = drinks
+    @stock = stock
   end
 
   def drinks_count()
@@ -17,6 +18,7 @@ attr_reader :name, :till
       if @drinks.include?(drink)
         customer.buy_drink(drink)
         @till += drink.price
+        change_stock(drink)
       end
     end
   end
@@ -24,6 +26,14 @@ attr_reader :name, :till
   def order_food(customer, food)
     customer.buy_food(food)
     @till += food.price
+  end
+
+  def change_stock(drink)
+    @stock[drink] -= 1
+  end
+
+  def stock_count(drink)
+    return @stock[drink]
   end
 
 end
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -12,7 +12,8 @@ class PubTest < MiniTest::Test
     @drink1 = Drink.new("Vodka", 2, 20)
     @drink2 = Drink.new("Beer", 1, 10)
     @drinks = [@drink1, @drink2]
-    @pub1 = Pub.new("Ricky's Bar", 5000, @drinks)
+    stock = {@drink1 => 100, @drink2=> 200}
+    @pub1 = Pub.new("Ricky's Bar", 5000, @drinks, stock)
     @customer = Customer.new("John", 40, 30)
 
     @food1 = Food.new("Burger", 3, 5)
@@ -44,4 +45,16 @@ class PubTest < MiniTest::Test
     assert_equal(5004, @pub1.till)
     assert_equal(5, @customer.drunkness_level)
   end
+
+  def test_change_stock
+    @pub1.order_drink(@customer, @drink2)
+    # @pub1.change_stock(@drink2)
+    assert_equal(39, @customer.wallet)
+    assert_equal(5001, @pub1.till)
+    assert_equal(10, @customer.drunkness_level)
+    assert_equal(199, @pub1.stock_count(@drink2))
+  end
+
+  
+
 end
