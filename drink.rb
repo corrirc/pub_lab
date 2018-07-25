9aa2bc0a2878c70724d1576acf3a04e47581db36
Ricky <rickycorrigan@icloud.com>
2018-07-25 13:19:31 +0100

    test pass for alchol level

--- drink.rb
+++ drink.rb
@@ -1,11 +1,11 @@
 class Drink
 
-  attr_reader :name, :price
+  attr_reader :name, :price, :alchol_level
 
-    def initialize(name, price)
+    def initialize(name, price, alchol_level)
       @name = name
       @price = price
-      
+      @alchol_level = alchol_level
     end
 
 end
--- specs/drink_spec.rb
+++ specs/drink_spec.rb
@@ -7,7 +7,7 @@ class DrinkTest < MiniTest::Test
 
 
   def setup()
-    @drink1 = Drink.new("Vodka", 2)
+    @drink1 = Drink.new("Vodka", 2, 20)
   end
 
   def test_name()
@@ -19,7 +19,9 @@ class DrinkTest < MiniTest::Test
     assert_equal(2, @drink1.price)
   end
 
-
+  def test_alchol_level()
+    assert_equal(20, @drink1.alchol_level)
+  end
 
 
 end
