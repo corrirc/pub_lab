ee39110c7723aecccee0bd714964b211a867a567
Ricky <rickycorrigan@icloud.com>
2018-07-25 13:52:53 +0100

    test pass food name

---
+++ specs/food_spec.rb
@@ -0,0 +1,19 @@
+require('minitest/autorun')
+require('minitest/rg')
+
+require_relative('../pub.rb')
+require_relative('../drink.rb')
+require_relative('../customer.rb')
+require_relative('../food.rb')
+
+class FoodTest < MiniTest::Test
+
+  def setup()
+    @food1 = Food.new("Burger", 3, 5)
+  end
+
+  def test_food_name
+    assert_equal("Burger", @food1.name)
+  end
+
+end
