71c382d977271a31b0cb07631a8ef73e0ec47e23
Ricky <rickycorrigan@icloud.com>
2018-07-25 11:34:53 +0100

    test pass for customer name

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -5,8 +5,13 @@ require_relative('../customer.rb')
 
 class CustomerTest < MiniTest::Test
 
-  
+  def setup()
+    @customer1 = Customer.new("Ricky", 50, 32)
+    @customer2 = Customer.new("Sithara", 60, 30)
+  end
 
-
+  def test_name()
+    assert_equal("Ricky", @customer1.name)
+  end
 
 end
